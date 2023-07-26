df3c94e7bf13e5d07274d5445d835393701eff6d
Ravin01 <ravinganapathi@gmail.com>
2023-07-27 01:22:04 +0530

    published the task

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div style={{
+            padding: '85px 20px 20px 20px',
+            backgroundColor: '#ECF0F1',
+            minHeight: '90vh'
+        }}>
+            <h3>About us</h3>
+        </div>
+    )
+}
+export default About;
