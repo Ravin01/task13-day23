556578b2f4359982f93cec01a6c8b74fc3e41f71
Ravin01 <ravinganapathi@gmail.com>
2023-08-05 12:16:09 +0530

    updated

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
             padding: '85px 20px 20px 20px',
             backgroundColor: '#ECF0F1',
             minHeight: '90vh'
-        }}>
+        }}> 
             <h3>Contact us</h3>
         </div>
     )
